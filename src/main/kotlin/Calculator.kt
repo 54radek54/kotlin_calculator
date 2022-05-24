461800b95bd86aaaefe9b0ac172a281bfbfc4f6d
54radek54 <81229833+54radek54@users.noreply.github.com>
2022-05-24 14:44:37 +0200

    Calculator updates v4

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -11,7 +11,7 @@ val VAL = arrayOf("x", "÷", ".", "^")
 val NUMBERS = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
 fun calculate(input: String): String? {
-    if (input == "Missing bracket" || input == "End of expression" || input == "Token is not a number" || input == "Token unknown" || input == "Division by zero") {
+        if (input == "Missing bracket" ||input == "Wrong expression!"|| input == "End of expression" || input == "Token is not a number" || input == "Token unknown" || input == "Division by zero") {
         return null
     }
     if ((CHECK.any { input.endsWith(it) })) {
@@ -32,7 +32,7 @@ fun onButtonClick(value: String, calculationValue: MutableState<TextFieldValue>)
     val input = calculationValue.value.text
     if (!(input === "" && VAL.any { value === it })) {
         if (!(value === "(" && NUMBERS.any { input.endsWith(it) || input.endsWith(".") })) {
-            if (input != "Missing bracket" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
+            if (input != "Missing bracket" && input != "Wrong expression!" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
                 if (!(CHECK.any { value == it } && CHECK.any { calculationValue.value.text.endsWith(it) })) {
                     if (value == ")") {
                         if (calculationValue.value.text.contains("(")) {
@@ -48,13 +48,18 @@ fun onButtonClick(value: String, calculationValue: MutableState<TextFieldValue>)
 }
 
 fun equal(calculationValue: MutableState<TextFieldValue>, expressionValue: MutableState<TextFieldValue>) {
-    if (!(CHECK.any { calculationValue.value.text.endsWith(it) })) {
-        expressionValue.value = TextFieldValue(calculationValue.value.text)
-        calculate(calculationValue.value.text)?.let { value ->
-            calculationValue.value = TextFieldValue(value)
+    val openBrackets = calculationValue.value.text.count { it == '(' }
+    val closingBrackets = calculationValue.value.text.count { it == ')' }
+    if (openBrackets == closingBrackets) {
+        if (!(CHECK.any { calculationValue.value.text.endsWith(it) })) {
+            expressionValue.value = TextFieldValue(calculationValue.value.text)
+            calculate(calculationValue.value.text)?.let { value ->
+                calculationValue.value = TextFieldValue(value)
+            }
         }
+    } else {
+        calculationValue.value = TextFieldValue("Wrong expression!")
     }
-
 }
 
 fun clear(calculationValue: MutableState<TextFieldValue>, expressionValue: MutableState<TextFieldValue>) {
@@ -64,7 +69,7 @@ fun clear(calculationValue: MutableState<TextFieldValue>, expressionValue: Mutab
 
 fun backspace(calculationValue: MutableState<TextFieldValue>) {
     val input = calculationValue.value.text
-    if (input != "Missing bracket" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
+    if (input != "Missing bracket" && input != "Wrong expression!" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
         if (calculationValue.value.text != "") {
             calculationValue.value = TextFieldValue(
                 if (calculationValue.value.text.length != 1) {
@@ -87,7 +92,7 @@ fun sinValue(calculationValue: MutableState<TextFieldValue>, expressionValue: Mu
 fun power(calculationValue: MutableState<TextFieldValue>) {
     val input = calculationValue.value.text
     if (!(input === "")) {
-        if (input != "Missing bracket" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
+        if (input != "Missing bracket" && input != "Wrong expression!" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
             if (!(CHECK.any { calculationValue.value.text.endsWith(it) })) {
                 calculationValue.value = TextFieldValue(calculationValue.value.text.plus("^"))
             }
@@ -96,26 +101,32 @@ fun power(calculationValue: MutableState<TextFieldValue>) {
 }
 
 fun root(calculationValue: MutableState<TextFieldValue>, expressionValue: MutableState<TextFieldValue>) {
-    if (calculate(calculationValue.value.text) != null) {
-        sqrt(calculate(calculationValue.value.text)!!.toDouble()).let { value ->
-            expressionValue.value = TextFieldValue("sqrt(".plus(calculationValue.value.text).plus(")"))
-            calculationValue.value = TextFieldValue(value.toString())
+    val input = calculationValue.value.text
+    if (input != "Missing bracket" && input != "Wrong expression!" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
+        if (calculate(calculationValue.value.text) != null) {
+            sqrt(calculate(calculationValue.value.text)!!.toDouble()).let { value ->
+                expressionValue.value = TextFieldValue("sqrt(".plus(calculationValue.value.text).plus(")"))
+                calculationValue.value = TextFieldValue(value.toString())
+            }
         }
     }
 }
 
 fun inverse(calculationValue: MutableState<TextFieldValue>, expressionValue: MutableState<TextFieldValue>) {
-    if (calculate(calculationValue.value.text) != null) {
-        (1 / calculate(calculationValue.value.text)!!.toDouble()).let { value ->
-            expressionValue.value = TextFieldValue("1/".plus(calculationValue.value.text))
-            calculationValue.value = TextFieldValue(value.toString())
+    val input = calculationValue.value.text
+    if (input != "Missing bracket" && input != "Wrong expression!" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
+        if (calculate(calculationValue.value.text) != null) {
+            (1 / calculate(calculationValue.value.text)!!.toDouble()).let { value ->
+                expressionValue.value = TextFieldValue("1/".plus(calculationValue.value.text))
+                calculationValue.value = TextFieldValue(value.toString())
+            }
         }
     }
 }
 
 fun signChange(calculationValue: MutableState<TextFieldValue>) {
     val input = calculationValue.value.text
-    if (input != "Missing bracket" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
+    if (input != "Missing bracket" && input != "Wrong expression!" && input != "End of expression" && input != "Token is not a number" && input != "Token unknown" && input != "Division by zero") {
         if (calculationValue.value.text != "") {
             if (calculationValue.value.text.startsWith("-")) {
                 calculationValue.value.text.removePrefix("-").let { value ->
